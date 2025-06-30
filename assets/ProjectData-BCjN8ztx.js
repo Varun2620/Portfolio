e186dc060628a9701adeef4674aeb8355b71c694
unknown <varunkumar262009@gmail.com>
2025-06-30 13:01:15 +0530

    Updates

---
+++ assets/ProjectData-BCjN8ztx.js
@@ -0,0 +1,21 @@
+const e="/assets/react-CHdo91hT.svg",t="/assets/notedi-CVx84iXm.jpg",a="/assets/memeGen-B3ZFYfH1.png",n="/assets/airBnb-9Jd8avtN.jpg",o="/assets/littleLemon--pxW5Ryp.jpg",i={userRecogintion:{projectName:"User Recognition For App Ratings",projectDesc:`
+                        Developed a user recognition system using Python to identify users and allow them to rate 
+                        applications, preventing bot reviews. Implemented a secure authentication mechanism to 
+                        verify user identities, resulting in a 10% reduction in fraudulent reviews.
+                     `,projectImage:e},noteEdi:{projectName:"Note Editor",projectDesc:`
+                        Developed a responsive web application for note editing using React, HTML, and CSS.
+                        Implemented an auto-save feature using local storage to prevent data loss, resulting 
+                        in a 99% data retention rate during unexpected browser closures.
+                     `,projectImage:t},littleLemon:{projectName:"Little Lemon Restaurant",projectDesc:`
+                        Developed a responsive restaurant web app using React and Figma for UI/UX design. Integrated 
+                        interactive table reservations, reducing booking friction and increasing test group engagement by 30%.
+                        Used component-based design for reusability and maintainability, simulating enterprise UI development.
+                     `,projectImage:o},memeGen:{projectName:"Meme Generator",projectDesc:`
+                        Developed a responsive meme generatot web application using React, HTML and CSS. Implemented by creating 
+                        the data for the memes locally and accessed it as a object. Used component based design for reuasbility 
+                        and maintanability.
+                     `,projectImage:a},airBnb:{projectName:"Air Bnb Clone",projectDesc:`
+                        Created a responsive Airbnb-style web interface with React.js, emphasising component-based
+                        architecture. Reusable elements (Navbar, Hero, and Card) were implemented, and dynamic 
+                        content was displayed by mapping over an array of data (appData).
+                     `,projectImage:n}};export{i as p};
